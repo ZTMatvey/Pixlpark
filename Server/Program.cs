04a4b5201dd724a04faff82f5715b551f1fd18d7
zenot <zenot@DESKTOP-N3F9INH>
2023-04-13 10:01:53 +0700

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("dev-pixlpark-ex", ExchangeType.Direct);
+var queueName = channel.QueueDeclare(durable: false, exclusive: false).QueueName;
+
+channel.QueueBind(queue: queueName,
+    exchange: "dev-pixlpark-ex",
+    routingKey: "code");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, e) =>
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[{DateTime.UtcNow}] {message}");
+};
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+Console.ReadLine();
